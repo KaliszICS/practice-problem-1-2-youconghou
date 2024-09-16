29756d535188519bbfb56014f269370c72cc0377
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-16 12:45:54 -0400

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -1,21 +1,30 @@
 
 
-#Write Assignment code here
+
 
 def q1():
+  #Write Assignment code here
 
 def q2():
+  #Write Assignment code here
 
 def q3():
+  #Write Assignment code here
 
 def q4():
+  #Write Assignment code here
 
 def q5():
+  #Write Assignment code here
 
 def q6():
+  #Write Assignment code here
 
 def q7():
+  #Write Assignment code here
 
+  
+#DO NOT EDIT ANYTHING AFTER THIS COMMENT
 q1()
 q2()
 q3()
